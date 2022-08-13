3d544f888b33b87810913660283e1b94d8922735
teretupiro <vafin_2005@list.ru>
2022-08-13 23:56:38 +0300

    turtle

---
+++ main.py
@@ -0,0 +1,34 @@
+import turtle
+
+turtlePen = turtle.Turtle()
+window = turtle.Screen()
+
+window.bgcolor("black")
+
+
+def polygon(n, size=80):
+    if n > 2:
+        angle = 360 / n
+
+        for n in range(0, n):
+            turtlePen.left(angle)
+            turtlePen.forward(size)
+
+
+turtlePen.speed(100)
+
+colors = ['orange', 'cyan', 'blue', 'green', 'red']
+
+size = 40
+
+for i in range(0, 60):
+    turtlePen.color(colors[i % 5])
+    polygon(4, size)
+    turtlePen.left(5)
+    size = size + 1
+    width=turtlePen.width(5)
+
+
+
+window.mainloop()
+
